23d82d6b0ac678a18521b7a882ff36bb8005dccf
Silent Movie House <silentmoviehouse@gmail.com>
2025-06-29 17:33:14 +0000

    Runtime Error Server
    TypeError: plugin is not a function
    
    src/ai/genki

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,6 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {ollama} from 'genkitx-ollama';
+import ollama from 'genkitx-ollama';
 
 export const ai = genkit({
   plugins: [
