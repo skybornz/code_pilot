1ffa0f020d9c0e7ce56ba7e74443399135bbec7b
Silent Movie House <silentmoviehouse@gmail.com>
2025-07-02 13:15:32 +0000

    Type '(LanguageSupport | ViewPlugin<(Anonymous class), undefined> | Face

--- src/components/codepilot/editor-panel.tsx
+++ src/components/codepilot/editor-panel.tsx
@@ -117,13 +117,13 @@ const DiffView = ({ original, modified, language, originalCommitHash, modifiedCo
     const originalExtensions = useMemo(() => [
         ...getLanguageExtension(language),
         lineHighlighter(originalLineClasses),
-        showMinimap,
+        showMinimap.of({}),
     ], [originalLineClasses, language]);
 
     const modifiedExtensions = useMemo(() => [
         ...getLanguageExtension(language),
         lineHighlighter(modifiedLineClasses),
-        showMinimap,
+        showMinimap.of({}),
     ], [modifiedLineClasses, language]);
 
 
@@ -202,7 +202,7 @@ export function EditorPanel({
   
   const extensions = useMemo(() => [
     ...getLanguageExtension(file.language),
-    showMinimap,
+    showMinimap.of({}),
   ], [file.language]);
   
   const activeCommitIndex = file.commits?.findIndex(c => c.hash === file.activeCommitHash) ?? -1;
